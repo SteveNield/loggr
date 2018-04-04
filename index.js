108c98475dcb77d2b3e629de809ba9e5c228b15a
stevenield <steve.nield@istein.ai>
2018-04-04 12:08:40 +0200

    Added support for application insights

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
-var moment = require('moment');
+var moment = require('moment'),
+    appinsights = require('applicationinsights');
 
-module.exports.info = function(message){
+function info(message){
   log('info', message);
 }
 
-module.exports.err = function(message){
+function err(message){
+  appInsightsError(message);
   log('error', message);
 }
 
-module.exports.warn = function(message){
+function warn(message){
   log('warning', message);
+}
+
+function appInsightsError(err){
+  if(appinsights.defaultClient){
+    appinsights.defaultClient.trackException({
+      exception: new Error(err)
+    })
+  }
+}
+
+function event(eventName, props){
+  if(appinsights.defaultClient){
+    info('Event '+eventName+' raised');
+    appinsights.defaultClient.trackEvent({
+      name: eventName,
+      properties: props
+    });
+  } else {
+    warn('event raised but appinsights is not configured');
+  }
 }
 
 function log(level, message){
     console.log(moment.utc(new Date()).toISOString() + ' [' + level + '] ' + message);
 }
+
+module.exports.setupAppInsights = function(instrumentationKey){
+  appinsights.setup(instrumentationKey).start();
+}
+
+module.exports.info = info;
+module.exports.warn = warn;
+module.exports.err = err;
+module.exports.event = event;
